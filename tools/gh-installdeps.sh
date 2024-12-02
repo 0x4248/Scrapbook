639ec7b7b9bd62d8dca65c7a7da0532ff4cfbc9c
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-12-02 23:38:31 +0000

    Format code
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- tools/gh-installdeps.sh
+++ tools/gh-installdeps.sh
@@ -10,7 +10,7 @@
 # Copyright (C) 2024 0x4248 and contributors
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the license is not changed.
-# 
+#
 # This software is free and open source. Licensed under the GNU general
 # public license version 3.0 as published by the Free Software Foundation.
 sudo apt install -y \
