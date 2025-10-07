63d152b3f73926e0f3a81fd28ec2a03b494165df
0x4248 <60709927+0x4248@users.noreply.github.com>
2025-10-07 18:27:59 +0100

    vh: remove random bloat
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- c/virtual_hardware/include/lib/libbus.h
+++ c/virtual_hardware/include/lib/libbus.h
@@ -16,24 +16,14 @@
 #include<stdbool.h>
 #include "lib/libbit.h"
 
-typedef bit bus_4[4];
+/* I know this is horrific */
 
-typedef bus_4 nibble;
+typedef bit bus_4[4];
 
 typedef struct {
     bit bus[8];
 } bus_8;
 
-typedef bus_8 byte;
-
 typedef struct {
     bit bus[16];
 } bus_16;
-
-typedef struct {
-    bit bus[32];
-} bus_32;
-
-typedef struct {
-    bit bus[64];
-} bus_64;
