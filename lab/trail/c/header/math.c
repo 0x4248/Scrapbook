9e7b714e2e103c73245d800c974fade8e43c83ea
0x4248 <60709927+0x4248@users.noreply.github.com>
2025-12-05 17:00:52 +0000

    trail > c/math.c: add NaN

--- lab/trail/c/header/math.c
+++ lab/trail/c/header/math.c
@@ -18,6 +18,7 @@
 
 #include <math.h>
 #include <stdio.h>
+#include <float.h>
 
 // ########## CHECKLIST ##########
 // Contents:
@@ -25,8 +26,8 @@
 // - Functions
 //      - Basic
 // ##### CONSTANTS #####
-// HUGE_VAL         [TODO]
-// INFINITY         [TODO]
+// HUGE_VAL         [X]
+// INFINITY         [X]
 // NAN              [TODO]
 // FP_FAST          [TODO]
 // FP_ILOGB0        [TODO]
@@ -43,21 +44,21 @@
  * https://en.cppreference.com/w/c/numeric/math/HUGE_VALL.html
  * https://learn.microsoft.com/en-us/cpp/c-runtime-library/huge-val-huge
  */
-
 void test_mathConsts(){
     /* HUGE_VAL - Its a big value, more specifically its the biggest float
      * value there is. Some people use it as an error e.g -HUGE_VAL.
      */
     printf("%f\n", HUGE_VAL); /* This just prints inf */
-    printf("%Lf\n", HUGE_VALL); /* HUGE_VAL but double */
+    printf("%Lf\n", HUGE_VALL); /* HUGE_VAL but double; so even huger value */
+
     /* Very similar to HUGE_VAL but can be used with isinf() */
     printf("%f\n", INFINITY);
+
+    /* NAN - Not a number, but is a float; can be used with nan() and isnan() */
+    printf("%f\n", NAN);
 }
 
 int main(){
     test_mathConsts();
-    /*
-     *
-     */
     return 0;
 }
