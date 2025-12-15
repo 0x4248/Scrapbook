0x4248/Scrapbook

commit 436643af30ba6a867fa537c819c68191bc192498

Make: add MacOS warning
Added a MacOS warning to let MacOS users know that nexus isnt supported
as well on MacOS

Why?

Well I test almost all of my code on Linux (Arch). But you might ask,
"You have a Mac?". Well, I SSH into a intranet server to do all my
development on.

Warnings added: WARNING: You are using Darwin (MacOS) which is not
usually supported by Nexus Set CONFIG_MAKE_IGNORE_WARNING_DARWIN=y to
suppress this warning

You can turn this of on make menuconfig (Top) -> Makefile options/Ignore
MacOS warning

Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

Makefile
@@ -13,11 +13,20 @@
 # This software is free and open source. Licensed under the GNU general
 # public license version 3.0 as published by the Free Software Foundation.
 
+
+
 all: precheck help
 
 NOCONFIG_TARGETS := menuconfig defconfig
 
 precheck:
+	@if [ "$$(uname -s)" = "Darwin" ]; then \
+	    if [ "$(CONFIG_MAKE_IGNORE_WARNING_DARWIN)" != "y" ]; then \
+	        echo "WARNING: You are using Darwin (MacOS) which is not usually supported by Nexus"; \
+	        echo "Set CONFIG_MAKE_IGNORE_WARNING_DARWIN=y to suppress this warning"; \
+	    fi; \
+	fi
+
 	$(T)$(LOG) -e "PRECHECK\tGLOBPATH"
 	$(T)pwd > .nexopath
 	$(T)$(LOG) -e "PRECHECK\tCONFIG"
@@ -27,6 +36,11 @@ precheck:
 			exit 1; \
 		fi; \
 	fi
+
+
+
+
+quickstart:
 
 
 -include tools/build/make/includes.mk

Kconfig
@@ -12,6 +12,11 @@ config MAKEFILE_LOGS
     help
       Prints a log message to explain what it is doing
 
+config CONFIG_MAKE_IGNORE_WARNING_DARWIN
+    bool "Ignore MacOS warning"
+    default n
+    help
+        Hides the MacOS warning
 endmenu
 
 menu "Documentation"

commit 408d454f59a370c015e8173bd5ff486f2639ac6b

[PATCH 436643a] makefile: Fix macos precheck eror
Fixes the makefile error when running the MacOS precheck.

Logs before the fix:
    make: *** [Makefile:27: precheck] Error 1

Fixes: 436643af30ba6a867fa537c819c68191bc192498
Reported-by: 0x4248 <60709927+0x4248@users.noreply.github.com>
Tested-by: Private internal testers <users@intranet>
Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

Makefile
@@ -20,12 +20,13 @@ all: precheck help
 NOCONFIG_TARGETS := menuconfig defconfig
 
 precheck:
-	@if [ "$$(uname -s)" = "Darwin" ]; then \
-	    if [ "$(CONFIG_MAKE_IGNORE_WARNING_DARWIN)" != "y" ]; then \
-	        echo "WARNING: You are using Darwin (MacOS) which is not usually supported by Nexus"; \
-	        echo "Set CONFIG_MAKE_IGNORE_WARNING_DARWIN=y to suppress this warning"; \
-	    fi; \
-	fi
+	@{ \
+		if [ "$$(uname -s)" = "Darwin" ] && \
+		   [ "$(CONFIG_MAKE_IGNORE_WARNING_DARWIN)" != "y" ]; then \
+			echo "WARNING: You are using Darwin (MacOS) which is not usually supported by Nexus"; \
+			echo "Set CONFIG_MAKE_IGNORE_WARNING_DARWIN=y to suppress this warning"; \
+		fi; \
+	} || true
 
 	$(T)$(LOG) -e "PRECHECK\tGLOBPATH"
 	$(T)pwd > .nexopath